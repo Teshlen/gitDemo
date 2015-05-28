1662a1e679f475b7afeb611f48efa8b11794d187
Uteshlen Nadesan <tnadesan@gmail.com>
2015-05-28 07:57:51 +0200

    new

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,15 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("commit 1 master");
-
-        System.out.println("commit 2 copy");
+        
 
 
-        System.out.println("commit 3 copy");
-
-        System.out.println("commit 4 copy");
-
-        System.out.println("commit 2 master");
     }
 }
