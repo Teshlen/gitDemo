ba342658787938a22de7826b4b01b7052530caac
Uteshlen Nadesan <tnadesan@gmail.com>
2015-05-28 11:10:48 +0200

    dsf

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("commit 1 master");
 
-=======
-        System.out.println("commit 1 master");  
->>>>>>> origin/master
     }
 }
